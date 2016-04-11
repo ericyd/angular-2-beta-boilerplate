e58ba379b8bcf7953962746b7505165fe9a36cbc
Eric Dauenhauer <eric_yd@yahoo.com>
2016-04-10 18:33:24 -0700

    resetting to default

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,22 @@
 import {Component} from 'angular2/core';
 
 @Component({
+    // this is how we embed this component in html.  It inserts the "template" instead of the directive/component tag.  We do this with
+    // <my-app></my-app>
     selector: 'my-app',
+    /* contains the html to insert for this component.
+    Can also be imported from external html file using templateUrl
+    Note: `` denote a multi-line string in TS.
+    All other components must live inside this component.  
+    */
     template: `
         <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
-    `,
+        <p>Hello World! yeah! THis is cool</p>
+    `
 })
+
+
+
 export class AppComponent {
 
 }
---
+++ self-notes.md
@@ -0,0 +1,9 @@
+# Folder explanations
+
+* app: contains the javascript files compiled from our typescript source files.  Do not touch!
+* assets: just contains scss code for now
+* dev: contains typescript files.  This is where we work most of the time.  Re-write typescript logic here.  boot.ts file starts our application (will be explained more later)
+* src: contains css files compiled from scss in assets.  Do not touch!
+* typings: can ignore it.  Contains info for typescript
+
+Note: the main file index.html is the only html file ever sent to the server.  Since angular apps are single page applications, everything else is done in the browser
