e20e150f8e47611ecdadf49f137c47fd4b0ef13a
Eric Dauenhauer <eric_yd@yahoo.com>
2016-04-11 21:48:19 -0700

    input-confirmation start

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,17 +1,30 @@
 import {Component} from 'angular2/core';
+import {InputComponent} from './bindings/input.component';
 
 @Component({
-    
     selector: 'my-app',
 
     template: `
-
+        <div class='container'>
+            <my-input (submitted)="onSubmit($event)"></my-input>
+        </div>
+        
+        <div class='container'>
+            {{myself.name}} {{myself.age}}
+        </div>
     `,
-    directives: []
+    directives: [InputComponent],
 })
 
 
-
+ 
 export class AppComponent {
+    myself = {name: '', age: ''};
+    confirmedMyself = {name: '', age: ''};
     
+    onSubmit(myself:any) {
+        for (let key in this.myself) {
+            this.myself[key] = myself[key];
+        }
+    }
 }
