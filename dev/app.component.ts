e0bec736b332605f05d49b86db794b00f3d002c2
Eric Dauenhauer <eric_yd@yahoo.com>
2016-04-11 17:32:54 -0700

    custom property binding

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,6 +3,7 @@ import {StringInterpolationComponent} from './string-interpolation.component';
 import {PropertyBindingComponent} from './property-binding.component';
 import {EventBindingComponent} from './event-binding.component';
 import {TwoWayDatabindingComponent} from './two-way-databinding.component';
+import {PropertyBinding2Component} from './property-binding2.component';
 
 @Component({
     // this is how we embed this component in html.  It inserts the "template" instead of the directive/component tag.  We do this with
@@ -15,17 +16,33 @@ import {TwoWayDatabindingComponent} from './two-way-databinding.component';
     */
     template: `
         <h1>Angular 2 Boilerplate</h1>
+        
         <p>Hello World! yeah! THis is cool</p>
+        
+        <section class="parent">
+        <h1>Property Binding 2 (in parent component)</h1>
+            <h4> Please enter your name</h4>
+            <input type="text" [(ngModel)]="name">
+            <br><br>
+            <section class="child">
+                <property-binding2 [myName]="name" [myAge]="26"></property-binding2>
+            </section>
+        </section>
+        
+        <h1>String Interpolation</h1>
         <string-interpolation></string-interpolation>
+        <h1>Property Binding 1</h1>
         <property-binding></property-binding>
+        <h1>Event binding</h1>
         <event-binding></event-binding>
+        <h1>Two way data binding</h1>
         <two-way-databinding></two-way-databinding>
     `,
-    directives: [StringInterpolationComponent, PropertyBindingComponent, EventBindingComponent, TwoWayDatabindingComponent]
+    directives: [StringInterpolationComponent, PropertyBindingComponent, EventBindingComponent, TwoWayDatabindingComponent, PropertyBinding2Component]
 })
 
 
 
 export class AppComponent {
-
+    name = '';
 }
