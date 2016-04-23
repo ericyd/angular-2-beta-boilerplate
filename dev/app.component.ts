aa5e044b66fa10ee592529e6fa21439e66ba78a1
Eric Dauenhauer <eric_yd@yahoo.com>
2016-04-23 11:46:18 -0700

    working on services

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,13 +1,15 @@
 import {Component} from 'angular2/core';
+import {Component1Component} from "./component1.component";
+import {Component2Component} from "./component2.component";
 
 @Component({
     
     selector: 'my-app',
 
     template: `
-
+        <component-1></component-1>
     `,
-    directives: []
+    directives: [Component1Component, Component2Component]
 })
 
 
---
+++ dev/component1.component.ts
@@ -0,0 +1,20 @@
+import {Component} from 'angular2/core';
+import {LoggingService} from "./services/logging.service";
+
+@Component({
+    selector: 'component-1',
+    template: `
+        <input type="text" #message>
+        <button (click)="onLog(message.value)">Send</button>
+    `,
+    providers: [LoggingService],
+})
+
+export class Component1Component {
+    //use the constructor for *injecting services*
+    constructor(private _loggingService: LoggingService) {}
+
+    onLog(message:string) {
+        this._loggingService.log(message);
+    }
+}
